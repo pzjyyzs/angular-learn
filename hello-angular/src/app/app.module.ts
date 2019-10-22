54fa5d600b7fee8180e53fae14abf248dad23ea0
pzjyyzs <463669395@qq.com>
2019-10-22 23:25:06 +0800

    route http

--- hello-angular/src/app/app.module.ts
+++ hello-angular/src/app/app.module.ts
@@ -2,20 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AuthService } from './core/auth.service';
+import { routing } from './app.routes';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    routing
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
