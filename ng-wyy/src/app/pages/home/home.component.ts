dbac119469125197acb01c1c338237c1b05a320f
pzjyyzs <463669395@qq.com>
2020-02-26 23:07:59 +0800

    轮播组件

--- ng-wyy/src/app/pages/home/home.component.ts
+++ ng-wyy/src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { HomeService } from 'src/app/service/home.service';
 import { Banner } from 'src/app/service/data-types/common.types';
+import { NzCardComponent, NzCarouselComponent } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-home',
@@ -9,15 +10,23 @@ import { Banner } from 'src/app/service/data-types/common.types';
 })
 export class HomeComponent implements OnInit {
 
+  carouselActiveIndex = 0;
   banners: Banner[];
+  @ViewChild(NzCarouselComponent, { static: true }) private nzCarousel: NzCarouselComponent;
   constructor(private homeServe: HomeService) {
     this.homeServe.getBanners().subscribe(banners => {
       this.banners = banners;
-      console.log(banners)
-    })
+    });
    }
 
   ngOnInit() {
   }
 
+  OnBeforeChange({ to }) {
+    this.carouselActiveIndex = to;
+  }
+
+  onChangeSlide(type: 'pre' | 'next') {
+    this.nzCarousel[type]();
+  }
 }
