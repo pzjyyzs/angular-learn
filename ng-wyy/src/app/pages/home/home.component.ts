36b178375eb892624b05eca7152bed1f9837ac56
pzjyyzs <463669395@qq.com>
2020-02-29 15:26:59 +0800

    resolve加载数据

--- ng-wyy/src/app/pages/home/home.component.ts
+++ ng-wyy/src/app/pages/home/home.component.ts
@@ -3,6 +3,8 @@ import { HomeService } from 'src/app/service/home.service';
 import { Banner, HotTag, SongSheet, Singer } from 'src/app/service/data-types/common.types';
 import { NzCardComponent, NzCarouselComponent } from 'ng-zorro-antd';
 import { SingerService } from 'src/app/service/singer.service';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs/internal/operators';
 
 @Component({
   selector: 'app-home',
@@ -17,36 +19,19 @@ export class HomeComponent implements OnInit {
   songSheetList: SongSheet[];
   singers: Singer[];
   @ViewChild(NzCarouselComponent, { static: true }) private nzCarousel: NzCarouselComponent;
-  constructor(private homeServe: HomeService, private singerServe: SingerService) {
-    this.getBanners();
-    this.getHotTags();
-    this.getPersonalizedSheetList();
-    this.getEnterSingers();
+
+  constructor(private route: ActivatedRoute) {
+    this.route.data.pipe(map(res => res.homeDatas)).subscribe(([banners, hotTags, songSheetList,singers]) =>{
+      this.banners = banners;
+      this.hotTags = hotTags;
+      this.songSheetList = songSheetList;
+      this.singers = singers;
+    })
   }
 
   ngOnInit() {
   }
 
-  private getBanners() {
-    this.homeServe.getBanners().subscribe(banners => {
-      this.banners = banners;
-    });
-  }
-  private getHotTags() {
-    this.homeServe.getHotTags().subscribe(tags => {
-      this.hotTags = tags;
-    });
-  }
-  private getPersonalizedSheetList() {
-    this.homeServe.getPerosonalSheetList().subscribe(sheets => {
-      this.songSheetList = sheets;
-    });
-  }
-  private getEnterSingers() {
-    this.singerServe.getEnterSinger().subscribe(singer => {
-      this.singers = singer;
-    })
-  }
   OnBeforeChange({ to }) {
     this.carouselActiveIndex = to;
   }
