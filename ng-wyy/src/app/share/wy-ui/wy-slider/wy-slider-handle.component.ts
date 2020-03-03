a5916447d2ac6f2641fa218e0bff72acfa413c51
pzjyyzs <463669395@qq.com>
2020-03-03 23:08:09 +0800

    遗漏

--- ng-wyy/src/app/share/wy-ui/wy-slider/wy-slider-handle.component.ts
+++ ng-wyy/src/app/share/wy-ui/wy-slider/wy-slider-handle.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, ChangeDetectionStrategy } from '@angular/core';
 import { WySliderStyle } from './wy-slider-types';
 
 @Component({
   selector: 'app-wy-slider-handle',
-  template: `<div class="wy-slider-handle" [ngStyle]="style"></div>`
+  template: `<div class="wy-slider-handle" [ngStyle]="style"></div>`,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WySliderHandleComponent implements OnInit, OnChanges {
   @Input() wyVertical = false;
