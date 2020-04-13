e830300ec51cea78518c83e4026035086b6ed166
pzjyyzs <463669395@qq.com>
2020-04-13 21:32:18 +0800

    登录弹窗u'r'l

--- ng-wyy/src/app/share/wy-ui/wy-ui.module.ts
+++ ng-wyy/src/app/share/wy-ui/wy-ui.module.ts
@@ -3,15 +3,18 @@ import { SingleSheetComponent } from './single-sheet/single-sheet.component';
 import { PlayCountPipe } from '../pipes/play-count.pipe';
 import { WyPlayerModule } from './wy-player/wy-player.module';
 import { WySearchModule } from './wy-search/wy-search.module';
+import { WyLayerModule } from './wy-layer/wy-layer.module';
+import { WyLayerModalComponent } from './wy-layer/wy-layer-modal/wy-layer-modal.component';
 
 
 
 @NgModule({
-  declarations: [SingleSheetComponent, PlayCountPipe],
+  declarations: [SingleSheetComponent, PlayCountPipe, WyLayerModalComponent],
   imports: [
     WyPlayerModule,
-    WySearchModule
+    WySearchModule,
+    WyLayerModule
   ],
-  exports: [SingleSheetComponent, PlayCountPipe, WyPlayerModule, WySearchModule]
+  exports: [SingleSheetComponent, PlayCountPipe, WyPlayerModule, WySearchModule, WyLayerModule]
 })
 export class WyUiModule { }
