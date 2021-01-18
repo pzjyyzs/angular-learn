65358539ec02a50a5e94ef2f06484a962a6245f7
pzjyyzs <463669395@qq.com>
2021-01-18 23:48:15 +0800

    ngmodule

--- ng-new/src/app/app.component.ts
+++ ng-new/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+// selector 普通html选择器 选中app-root的标签
+// templateUrl 组件的模板
+// styleUrls 样式的文件
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- ng-new/src/app/app.module.ts
+++ ng-new/src/app/app.module.ts
@@ -3,7 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// 这是一个类 被@NgModule这个装饰器给装饰 就称为angular module
+// 这个装饰器 接受一个对象作为参数  这个对象在angular中 被称为元数据
+// 其中的四个属性是用得最多的
+// declarations 数组  告诉angular 哪些组件、指令、管道属于这个模块
+// 每个组件只能声明在一个Module里 使用得组件必须声明在一个模块中
 
+// imports 导入其他ng module 可以是自己写的 也可以是内置的
+// providers 给外界或者自己提供一些服务
+// bootstrap 根模块中使用 指定整个项目的根组件
 @NgModule({
   declarations: [
     AppComponent
@@ -15,4 +23,4 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { }  
