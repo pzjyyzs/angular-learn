b13a2d4d5f42a0fcbed41455b67bcab0fee5e22a
junyu.wei <463669395@qq.com>
2021-02-11 16:49:04 +0800

    CanActivate

--- ng-new/src/app/app.module.ts
+++ ng-new/src/app/app.module.ts
@@ -17,6 +17,8 @@ import { ExmpleComponent } from './rxjs/exmple/exmple.component';
 import { HeroListComponent } from './router/hero-list/hero-list.component';
 import { HeroModule } from './router/hero.module';
 import { CrisisCenterModule } from './router/crisis-center/crisis-center.module';
+import { AdminModule } from './router/admin/admin.module';
+import { AuthModule } from './router/auth/auth.module';
 // 这是一个类 被@NgModule这个装饰器给装饰 就称为angular module
 // 这个装饰器 接受一个对象作为参数  这个对象在angular中 被称为元数据
 // 其中的四个属性是用得最多的
@@ -44,6 +46,8 @@ import { CrisisCenterModule } from './router/crisis-center/crisis-center.module'
     BrowserModule,
     HeroModule,
     CrisisCenterModule,
+    AdminModule,
+    AuthModule,
     FormsModule,
     AppRoutingModule
   ],
