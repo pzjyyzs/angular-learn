acbaad0219f6e9168da80d2cdb6fe8285fd5ad07
pzjyyzs <463669395@qq.com>
2021-03-01 22:56:28 +0800

    动画

--- ng-new/src/app/heroes/login/login.module.ts
+++ ng-new/src/app/heroes/login/login.module.ts
@@ -5,10 +5,12 @@ import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { FormsModule } from '@angular/forms';
 import { OpenCloseComponent } from './open-close/open-close.component';
+import { FlyInOutComponent } from './fly-in-out/fly-in-out.component';
+import { InsertRemoveComponent } from './insert-remove/insert-remove.component';
 
 
 @NgModule({
-  declarations: [LoginComponent, OpenCloseComponent],
+  declarations: [LoginComponent, OpenCloseComponent, FlyInOutComponent, InsertRemoveComponent],
   imports: [
     CommonModule,
     FormsModule,
