4ca9caaf4bccd94e26550f9a88bc183f16820d7a
pzjyyzs <463669395@qq.com>
2022-10-22 15:04:47 +0800

    form

--- heroes/src/app/app.module.ts
+++ heroes/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { MessageComponent } from './message/message.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { HeroFormComponent } from './hero-form/hero-form.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
     HeroDetailComponent,
     MessageComponent,
     DashboardComponent,
-    HeroSearchComponent
+    HeroSearchComponent,
+    HeroFormComponent
   ],
   imports: [
     BrowserModule,
