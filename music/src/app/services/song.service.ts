f883c3849625e4dbd8212e52a68c18cd40a29697
pzjyyzs <463669395@qq.com>
2022-11-12 20:45:48 +0800

    index ui

--- music/src/app/services/song.service.ts
+++ music/src/app/services/song.service.ts
@@ -1,7 +1,29 @@
+import { Dj, Singer } from './data-types';
 import { map, Observable, combineLatest } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { API_CONFIG, ServicesModule } from './services.module';
+import *  as queryString from 'query-string';
+
+type SingerParams = {
+  type?: number;
+  area: number;
+  limit: number;
+}
+
+type DjParams = {
+  limit: number;
+}
+
+const defaultParams: SingerParams = {
+  type: 1,
+  area: 7,
+  limit: 5,
+}
+
+const djParams: DjParams = {
+  limit: 5,
+}
 
 @Injectable({
   providedIn: ServicesModule
@@ -13,5 +35,17 @@ export class SongService {
   getSheet(id: number): Observable<any> {
     return this.http.get<any[]>(this.url + `/playlist/detail?id=${id}`)
       .pipe(map(res => res));
+  }
+
+  getIndexSongList(args: SingerParams = defaultParams): Observable<Singer[]> {
+    const params = new HttpParams({ fromString: queryString.stringify(args) });
+    return this.http.get<{ artists: Singer[] }>(this.url + '/artist/list', { params })
+      .pipe(map((res: { artists: Singer[] }) => res.artists));
+  }
+
+  getTopListDj(args: DjParams = djParams): Observable<Dj[]> {
+    const params = new HttpParams({ fromString: queryString.stringify(args) });
+    return this.http.get<{ data: { list: Dj[] }}>(this.url + '/dj/toplist/popular', { params })
+      .pipe(map((res: { data: { list: Dj[] }}) => res.data.list));
   }
 }
