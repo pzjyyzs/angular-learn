959f0eeebb761be136c72e7a67dc1c8cdda10c4c
pzjyyzs <463669395@qq.com>
2023-01-11 00:37:18 +0800

    user ngrx

--- music/src/app/pages/home/home.component.ts
+++ music/src/app/pages/home/home.component.ts
@@ -1,4 +1,8 @@
+import { SetUser } from './../../store/actions/user.action';
+import { UserService } from 'src/app/services/user.service';
 import { Component, OnInit } from '@angular/core';
+import { StoreIndexModule } from 'src/app/store/store.module';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService, private store$: Store<StoreIndexModule>) { }
 
   ngOnInit(): void {
+    const cookie = localStorage.getItem('cookie');
+    this.userService.getLoginStatus(cookie).subscribe(data => {
+      if (data.code === 200) {
+        this.store$.dispatch(SetUser({ user: data.profile }));
+      }
+    })
   }
 
 }
