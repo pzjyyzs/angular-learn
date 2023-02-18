b24b682489aa933ae58397432cce52f04147a84c
pzjyyzs <463669395@qq.com>
2023-02-19 00:24:59 +0800

    base 49 - 50

--- base/src/app/admin/containers/donut-form/donut-form.component.ts
+++ base/src/app/admin/containers/donut-form/donut-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Donut } from './../../models/donut';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -8,6 +9,8 @@ import { NgForm } from '@angular/forms';
 })
 export class DonutFormComponent implements OnInit {
 
+  @Input() donut!: Donut;
+  @Output() create = new EventEmitter<Donut>();
   icons: string[] = [
     'caramel-swirl',
     'glazed-fudge',
@@ -24,7 +27,7 @@ export class DonutFormComponent implements OnInit {
 
   handleSubmit(form: NgForm) {
     if (form.valid) {
-
+      this.create.emit(form.value);
     } else {
       form.form.markAllAsTouched();
     }
--- base/src/app/admin/containers/donut-single/donut-single.component.ts
+++ base/src/app/admin/containers/donut-single/donut-single.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Donut } from '../../models/donut';
 
 @Component({
   selector: 'app-donut-single',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonutSingleComponent implements OnInit {
 
+  donut!: Donut;
   constructor() { }
 
   ngOnInit(): void {
+    this.donut = {
+      id: 'y8z0As',
+      name: 'Just Chocolate',
+      icon: 'just-chocolate',
+      price: 119,
+      promo: 'limited',
+      description: 'For the pure chocoholic.'
+    }
   }
 
+  onCreate(donut: Donut) {
+
+  }
 }
